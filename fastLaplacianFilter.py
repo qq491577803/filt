a531a8e2d5660e6172426e14132ecda86c793180
qq491577803 <491577803@qq.com>
2020-11-08 20:59:07 +0800

    completed

--- fastLaplacianFilter.py
+++ fastLaplacianFilter.py
@@ -1,35 +1,33 @@
+#！usr/bin/env python
+#encoding:utf-8
+'''
+__Author__:Lsz
+Function:Image detail enhance
+Author Loca:BeiJing China Haidian
+Email:www.491577803@qq.com
+Berif:Use Python base Lang
+Todo:Use Numpy to optim process speed or c++
+'''
 import numpy as np
 import cv2
 import math
 import matplotlib.pyplot as plt
-
+import time
 class FastLaplacianFilter():
-
     def __init__(self,path:str,sigma:float,alpha:float,beta:float,noise:float,gamma:float):
         self.sigma = sigma
         self.alpha = alpha
         self.beta = beta
         self.noise = noise
         self.gamma = gamma
-
-        self.maxPyramidNum = 50
+        self.maxPyramidNum = 128
         self.imgRGB = cv2.imread(path,cv2.IMREAD_UNCHANGED)
         self.imgRGB = self.imgRGB.astype(np.float64)
-        self.imgRGB = cv2.resize(self.imgRGB,dsize=(266,256))
+        self.imgRGB = cv2.resize(self.imgRGB,dsize=(128,128))
         # rgb to gray [b,g,r]  or use cv2.cvtColor
         self.imgGray = self.rgb2gray(self.imgRGB)
         self.maxLeves = math.floor(math.log2(min(self.imgGray.shape[0],self.imgGray.shape[1])))
         self.imgGray = self.imgGray.astype(np.float64)
-        filtedImg = self.fiter2D(self.imgGray,"upSample")
-        self.imshow(filtedImg)
-
-
-
-        #log domin imgGray
-        for  i in range(self.imgGray.shape[0]):
-            for j in range(self.imgGray.shape[1]):
-                self.imgGray[i][j] = math.log(self.imgGray[i][j] + 1.0)
-
 
     def rgb2gray(self,image):
         rgb2yCoeff = [0.0722,0.7152,0.2126]
@@ -60,30 +58,15 @@ class FastLaplacianFilter():
         return np.array(kernel,dtype=np.float64)
     def fiter2D(self,inImg,sampleType):
         if sampleType == "downSample":
-            kernel = [
-                1 / 256, 4 / 256, 6 / 256, 4 / 256, 1 / 256,
-                4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256,
-                6 / 256, 24 / 256, 36 / 256, 24 / 256, 6 / 256,
-                4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256,
-                1 / 256, 4 / 256, 6 / 256, 4 / 256, 1 / 256
-            ]
+            kernel = self.downSampleKernel()
         if sampleType == "upSample":
-            kernel = [
-                1/64, 4/64,  6/64,  4/64,   1/64,
-                4/64, 16/64, 24/64, 16/64,  4/64,
-                6/64, 24/64, 36/64, 24/64,  6/64,
-                4/64, 16/64, 24/64, 16/64,  4/64,
-                1/64, 4/64,  6/64,  4/64,   1/64,
-        ]
-
+            kernel = self.upSampleKernel()
         kernel = np.array(kernel,dtype=np.float64)
         outImg = np.zeros_like(inImg,dtype=np.float64)
         height = inImg.shape[0]
         width = inImg.shape[1]
-
         width2 =  (width - 1) << 1
         height2 = (height- 1) << 1
-
         ksizeY = 5 >> 1
         ksizeX = 5 >> 1
         idx = 0
@@ -91,7 +74,6 @@ class FastLaplacianFilter():
             for x in range(width):
                 sum = 0.0
                 kIdx = 0
-
                 for yy in range(y-ksizeY,y+ksizeY + 1):
                     yyC = yy
                     if yyC >= height:
@@ -124,16 +106,17 @@ class FastLaplacianFilter():
                 diff = imgGray[i][j] - mean
                 diffAbs = abs(diff)
                 sign = 1 if diff >= 0 else -1
-                if(diffAbs - sigma):
+                if(diffAbs > sigma):
                     out[i][j] = mean + sign * ((diffAbs - sigma) * beta + sigma)
                 else:
                     out[i][j] = mean + sign * math.pow(diffAbs / sigma,alpha) * sigma
         return out
+
     #down sample image
     def downSample(self,inImg, rows, cols):
         outImg = np.zeros(shape=(rows, cols), dtype=np.float64)
-        # outImg = cv2.filter2D(outImg, -1, self.downSampleKernel())
-        outImg = self.fiter2D(outImg,"downSample")
+        outImg = cv2.filter2D(outImg, -1, self.downSampleKernel())
+        # outImg = self.fiter2D(outImg,"downSample")
         xRow = 0
         for row in range(rows):
             yCol = 0
@@ -154,8 +137,8 @@ class FastLaplacianFilter():
                 upSample[row][col] = inImg[xRow][yCol]
                 yCol += 1
             xRow += 1
-        # upSample = cv2.filter2D(upSample, -1, kernel=self.upSampleKernel())
-        upSample = self.fiter2D(upSample,"upSample")
+        upSample = cv2.filter2D(upSample, -1, kernel=self.upSampleKernel())
+        # upSample = self.fiter2D(upSample,"upSample")
         return upSample
 
     #build laplacian Pyramid
@@ -169,7 +152,6 @@ class FastLaplacianFilter():
             downSampleImg = self.downSample(leveImg,rows,cols)
             upSampleImg = self.upSample(downSampleImg,leveImg.shape[0],leveImg.shape[1])
             gaussionPyramid[leve] = downSampleImg
-
             laplacianPyramid[leve - 1] = np.subtract(leveImg,upSampleImg)
         laplacianPyramid[maxLeves - 1] = gaussionPyramid[maxLeves - 1]
         return gaussionPyramid,laplacianPyramid
@@ -178,12 +160,15 @@ class FastLaplacianFilter():
         maxPyramidNum = len(means)
         laplacianLayers = [None] * maxPyramidNum # 50 * 6 layers
         for i in range(maxPyramidNum):
+            # self.imshow(self.imgGray)
             tmp = self.remapLum(self.imgGray,means[i],self.alpha,self.beta,self.sigma)
+            # self.imshow(tmp)
             #build gaussion and laplacian pyramid
             gaussionPyramid, laplacianPyramid = [],[]
             gaussionPyramid, laplacianPyramid = self.buildLaplacianPyramid(tmp,gaussionPyramid,laplacianPyramid,leves)
             laplacianLayers[i] = laplacianPyramid
         return laplacianLayers
+
     def rebuildLaplacianPyramid(self,lapLacianPyramid):
         maxLeve = len(lapLacianPyramid)
         outImg = lapLacianPyramid[maxLeve - 1]
@@ -192,6 +177,7 @@ class FastLaplacianFilter():
             upSampleImg = self.upSample(outImg,rows,cols)
             outImg = np.add(upSampleImg,lapLacianPyramid[leve -1])
         return outImg
+
     def clipBit(self,inPut,min,max):
         if min > max :
             return "error"
@@ -239,8 +225,8 @@ class FastLaplacianFilter():
                     laplacianPyramid[leve][row][col] = Blended
         print("Finally lapLacianPyramid length = {0}".format(len(laplacianPyramid)))
         outImg = self.rebuildLaplacianPyramid(laplacianPyramid)
-        print("outImg.shape:{0}",outImg.shape)
         return outImg
+
     #recovery img from log domin to rgb
     def recoverRgbImage(self,inImage):
         imgGray = inImage.copy()
@@ -253,11 +239,11 @@ class FastLaplacianFilter():
                 BGR = [0.00,0.00,0.00]
                 for c in range(self.imgRGB.shape[2]):
                     BGR[c] = imgGray[row][col] - minVal
-                    BGR[c] = BGR[c] + self.gamma * (math.log(self.imgRGB[row][col][c] + 1.0) - self.imgGray[row][col])
+                    BGR[c] = BGR[c] + self.gamma * (self.imgRGB[row][col][c] - self.imgGray[row][col])
                     BGR[c] = BGR[c] / interV
-                    BGR[c] = math.exp(BGR[c])
                 outImage[row][col][0],outImage[row][col][1],outImage[row][col][2] = BGR[0],BGR[1],BGR[2]
         return outImage
+
     def normalizeImgRgb(self,inImgRgb):
         norImgRgb = np.zeros_like(inImgRgb,dtype=np.float64)
         imgGray = inImgRgb.copy()
@@ -284,31 +270,44 @@ class FastLaplacianFilter():
                 imgNormed[row][col] = math.exp(inImgGray[row][col])
         self.imshow(imgNormed)
 
-
+    def clipImage(self,inImg):
+        outImg = np.zeros_like(inImg)
+        for row in range(inImg.shape[0]):
+            for col in range(inImg.shape[1]):
+                for c in range(inImg.shape[2]):
+                    tmp = self.clipBit(inImg[row][col][c],0,255)
+                    outImg[row][col][c] = tmp
+        return outImg
 
     def imageProcesser(self):
         means = self.getMeans(self.imgGray,self.maxPyramidNum)
         # build remap laplayers 50 * 6
         laplacianPyramids = self.remapLaplacianPyramid(self.imgGray,means,self.maxLeves)
         outImgGray = self.fastLoalConstrast(self.imgGray,means,laplacianPyramids)
-        outImgRgb = self.recoverRgbImage(outImgGray)
-        norImgRgb = self.normalizeImgRgb(outImgRgb)
-        # norImgRgb = self.grayImgNorm(outImgGray)
-        resImgRgb = np.zeros_like(norImgRgb,dtype=np.uint8)
-        for row in range(norImgRgb.shape[0]):
-            for col in range(norImgRgb.shape[1]):
-                for c in range(norImgRgb.shape[2]):
-                    tmp = norImgRgb[row][col][c]
-                    tmp = self.clipBit(tmp,0,255)
-                    resImgRgb[row][col][c] = np.uint8(tmp)
-        cv2.imshow("resImage",resImgRgb)
+
+        #show res Laplacian filter resoult
+        # cv2.imshow("resImg",outImgGray.astype(np.uint8))
+        # cv2.imshow("srcGray", self.imgGray.astype(np.uint8))
+        # cv2.waitKey(0)
+        #trans from gray2 rgb and apply gamma
+        imgRecorv = self.recoverRgbImage(outImgGray)
+        # cv2.imshow("srcRGBrecorv", (imgRecorv * 255).astype(np.uint8))
+        # cv2.waitKey(0)
+        #normalize 1bit to 8bit
+        norImgRgb = self.normalizeImgRgb(imgRecorv)
+        # cv2.imshow("norImgRgb",norImgRgb.astype(np.uint8))
+        # cv2.waitKey(0)
+        #clip img to 0 ~  255
+        resImgRgb = self.clipImage(norImgRgb)
+        cv2.imwrite("./srcRgb_gamma_{0}_alpha_{1}_beta_{2}_sigma_{3}_num_{4}_leve_{5}.png"\
+                    .format(self.gamma,self.alpha,self.beta,self.sigma,self.maxPyramidNum,self.maxLeves),\
+                    self.imgRGB.astype(np.uint8))
+        cv2.imwrite("./srcRgb_gamma_{0}_alpha_{1}_beta_{2}_sigma_{3}_num_{4}_leve_{5}.png"\
+                    .format(self.gamma,self.alpha,self.beta,self.sigma,self.maxPyramidNum,self.maxLeves), \
+                    resImgRgb.astype(np.uint8))
+        cv2.imshow("srcRgb",self.imgRGB.astype(np.uint8))
+        cv2.imshow("resImgRgb",resImgRgb.astype(np.uint8))
         cv2.waitKey(0)
-
-        cv2.imwrite("./outImage.png",norImgRgb)
-        print("outImage:",norImgRgb)
-        self.imshow(norImgRgb)
-
-
 
     def testLaplacian(self):
         path = r"E:\local_laplacian_filter\my_laplacian\data\inputdata\flower.png"
@@ -316,21 +315,19 @@ class FastLaplacianFilter():
         gray = cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
         guassionPyramid, laplacianPyramid = [],[]
         gaussPyramid,laplacianPyramid = self.buildLaplacianPyramid(gray, guassionPyramid, laplacianPyramid, 7)
-
         outImg = self.rebuildLaplacianPyramid(laplacianPyramid)
-
         self.imshow(outImg)
-
-
-
-
     def imshow(self,image):
         cv2.imshow("image",image.astype(np.uint8))
         cv2.waitKey(0)
 
 if __name__ == '__main__':
+    sTime = time.time()
     path = r"E:\local_laplacian_filter\my_laplacian\data\inputdata\flower.png"
-    fastLapcian = FastLaplacianFilter(path=path,sigma=0.9,alpha=1.0,beta=0.0,noise=0.01,gamma=1.2).imageProcesser()
+    fastLapcian = FastLaplacianFilter(path=path,sigma= 50,alpha= 1.5,beta=1,noise=0.01,gamma=1.2).imageProcesser()
+    eTime = time.time()
+    print("Total cost time :{0}".format(eTime - sTime))
     # fastLapcian = FastLaplacianFilter(path=path, sigma=0.9, alpha=1.0, beta=0.0, noise=0.01, gamma=1.2).testLaplacian()
+
 
 
